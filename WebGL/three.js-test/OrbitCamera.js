8ccfa8e15e219269a8c304f877c57e91ef0f29b8
makoto-lee <61498823+makoto-lee@users.noreply.github.com>
2022-03-18 00:43:27 +0800

    Add files via upload

---
+++ WebGL/three.js-test/OrbitCamera.js
@@ -0,0 +1,90 @@
+export class OrbitCamera {
+    #canvas = null;
+    #dragging = false;
+    #x;
+    #y;
+    #dx;
+    #dy;
+    #vx = 0;
+    #vy = 0;
+    #theta = 0;
+    #phi = 0;
+    #toRadian = Math.PI / 180;
+    #friction_coef = 0.9;
+
+    constructor(camera, lookat, radius, canvas = null, dragable = true, auto_move = true) {
+        this.camera = camera;
+        this.lookat = lookat;
+        this.radius = radius;
+        this.dragable = dragable;
+        // ==
+        this.auto_move = auto_move;
+        this.speed = 0.2;
+        this.view_point_max = 80;
+        this.view_point_min = -80;
+
+        if (dragable) {
+            this.#canvas = canvas;
+
+            this.#canvas.addEventListener("mousedown",
+                (event) => {
+                    this.#x = event.offsetX;
+                    this.#y = event.offsetY;
+                    this.#dragging = true;
+                });
+
+            this.#canvas, addEventListener("mousemove",
+                (event) => {
+                    if (this.#dragging) {
+
+                        this.#dx = event.offsetX - this.#x;
+                        this.#dy = event.offsetY - this.#y;
+                        this.#x = event.offsetX;
+                        this.#y = event.offsetY;
+
+                        this.#vx = this.#dx;
+                        this.#vy = this.#dy;
+
+                        console.log("offsetX: " + event.offsetX + " offsetY: " + event.offsetX);
+                    }
+                });
+
+            this.#canvas, addEventListener("mouseup",
+                (event) => {
+                    //this.#dx = 0;
+                    //this.#dy = 0;
+                    this.#dragging = false;
+                    //console.log("dx: " + this.#dx + " dy: " + this.#dy);
+                });
+        }
+    }
+
+    update() {
+        //console.log("dx: " + this.#dx + " dy: " + this.#dy);
+
+        // momentum
+        this.#theta += this.#vy * this.speed;
+        this.#phi -= this.#vx * this.speed;
+
+        // handle max-minimum view point
+        this.#theta = Math.max(this.#theta, this.view_point_min);
+        this.#theta = Math.min(this.#theta, this.view_point_max);
+
+        if (this.dragable) {
+            this.#vx *= this.#friction_coef;
+            this.#vy *= this.#friction_coef;
+        }
+
+        if (!this.#dragging & this.auto_move) {
+            this.#phi += 0.5;
+        }
+
+        // change camera position
+        this.camera.position.x = this.radius * Math.sin((this.#theta * -1 + 90) * this.#toRadian) * Math.sin(this.#phi * this.#toRadian);
+        this.camera.position.y = this.radius * Math.cos((this.#theta * -1 + 90) * this.#toRadian);
+        this.camera.position.z = this.radius * Math.sin((this.#theta * -1 + 90) * this.#toRadian) * Math.cos(this.#phi * this.#toRadian);
+
+        // refresh camera lookat
+        this.camera.lookAt(this.lookat);
+    }
+}
---
+++ WebGL/three.js-test/main.js
@@ -0,0 +1,78 @@
+import * as THREE from "https://cdn.skypack.dev/three@0.132.2";
+import { GLTFLoader } from "https://cdn.skypack.dev/three@0.132.2/examples/jsm/loaders/GLTFLoader.js";
+import { OrbitCamera } from "./OrbitCamera.js"
+
+/**
+ * set up scene, camera, renderer
+ */
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+
+const canvas = document.getElementById("myCanvas");
+if (!window.WebGLRenderingContext) {
+    alert("Sorry, your browser doesn`t support WebGL");
+}
+const renderer = new THREE.WebGLRenderer({ canvas: canvas });
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+/**
+ * set up background
+ */
+const background = new THREE.TextureLoader().load("./stone_brick_wall/textures/stone_brick_wall_001_diff_4k.jpg");
+scene.background = background;
+
+/**
+ * load WetFloorSign
+ */
+
+const signTexture = new THREE.TextureLoader().load("./WetFloorSign/textures/WetFloorSign_01_diff_1k.jpg");
+
+const signMaterial = new THREE.MeshBasicMaterial({ map: signTexture });
+
+const loader = new GLTFLoader();
+loader.load("./WetFloorSign/WetFloorSign_01_1k.gltf",
+    (gltf) => {
+        gltf.scene.traverse((child) => {
+            if (child.isMesh) {
+                const m = child;
+                m.receiveShadow = true;
+                m.castShadow = true;
+                m.material = signMaterial;
+                m.scale.set(3, 3, 3);
+                scene.add(m);
+            }
+        });
+    },
+    (xhr) => {
+        console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+    },
+    (error) => {
+        console.log("Error happend when loading GLTF");
+    });
+
+//const sign = new THREE.Mesh(geometry, material);
+
+//scene.add(sign);
+//camera.position.z = 5;
+//camera.position.y = 0;
+
+let oc = new OrbitCamera(camera, new THREE.Vector3(0, 0, 0), 5, canvas);
+oc.auto_move = false;
+
+
+/**
+ * start rendering
+ */
+function animate() {
+    requestAnimationFrame(animate);
+
+    //signMesh.rotation.y += 0.01;
+    //camera.position.y += 0.01;
+    //camera.lookAt(0, 0, 0);
+    oc.update();
+
+    renderer.render(scene, camera);
+};
+
+animate();
